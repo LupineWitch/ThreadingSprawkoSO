70838a40dc32a7362f190f92373fcd95b0a9b523
Vekz <V4nII@hotmail.com>
2020-06-14 11:17:51 +0200

    Safe exiting

--- WritersReadersSolution.c
+++ WritersReadersSolution.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <signal.h>
 #include <unistd.h>
 
-#define SLEEPTIME 0
+#define SLEEPTIME 10
+
+static volatile int keepRunning = 1; //Flag for exiting from application
 
 sem_t in; //Queue to be served
 sem_t mx; //Access to ReadersIn
@@ -22,7 +25,7 @@ pthread_cond_t WriteNow; //Condition on what to wait for logging
 void *Logger() //Logger thread to synchronize writing
 {
         pthread_mutex_lock(&MainLock);
-    while(1)
+    while(keepRunning)
     {
         pthread_cond_wait(&WriteNow, &MainLock);
         printf("\nReadersQ: %d WritersQ: %d [in: R:%d W:%d]", ReadersWaiting,WritersWaiting,ReadersIn,WritersIn);
@@ -33,7 +36,7 @@ void *Logger() //Logger thread to synchronize writing
 void* Reader(void* params)
 {
     int id = (int)(intptr_t)params; //Get ID of the Reader
-    while(1)
+    while(keepRunning)
     {
         ReadersWaiting++;
         pthread_cond_signal(&WriteNow);
@@ -73,7 +76,7 @@ void* Reader(void* params)
 void* Writer(void* params)
 {
     int id = (int)(intptr_t)params; //Get ID of the Writer
-    while(1)
+    while(keepRunning)
     {
         WritersWaiting++;
         pthread_cond_signal(&WriteNow);
@@ -98,6 +101,11 @@ void* Writer(void* params)
     }
 }
 
+void exit_handler()
+{
+    keepRunning=0;
+}
+
 int main(int argc, char* argv[])
 {
     //Program won't work if number of arguments is different than 2 (+1 becasue name of program is also an argument)
@@ -106,6 +114,9 @@ int main(int argc, char* argv[])
         printf("Sposob wywolania: 3 <liczba czytelnikow> <liczba pisarzy>\n");
         return -1;
     }
+
+    //Catching SIGINT to safely kill application
+    signal(SIGINT, exit_handler);
 
     //Declare table for Readers threads
     int NumR = atoi(argv[1]);
@@ -148,7 +159,8 @@ int main(int argc, char* argv[])
         pthread_create(&Writers[i], NULL, &Writer, (void*)(intptr_t)i);
     }
 
-    //Wait for threads to finish
+    //Wait for threads to finish and freeing the memory
+    pthread_join(Logger_th, NULL);
     for(int i=0; i<NumR; i++)
     {
         pthread_join(Readers[i], NULL);
@@ -157,6 +169,13 @@ int main(int argc, char* argv[])
     {
         pthread_join(Writers[i], NULL);
     }
+    
+    //Freeing the semaphores, mutexes and CVs
+    sem_destroy(&in);
+    sem_destroy(&mx);
+    sem_destroy(&wrt);
+    pthread_mutex_destroy(&MainLock);
+    pthread_cond_destroy(&WriteNow);
 
     return 0;
 }
